4f3e6d5aefd3776c4d52e0eee64b10f5cf2f8a36
stopachka <stepan.p@gmail.com>
2024-09-12 11:59:01 -0700

    roar

--- clojure_server/src/undertow_websockets/core.clj
+++ clojure_server/src/undertow_websockets/core.clj
@@ -5,15 +5,17 @@
 
 (defn handle-ws [_req]
   {:undertow/websocket
-   {:on-message (fn [{:keys [_channel _data]}]
-                  (println "[ws] open!"))
+   {:on-open (fn [_]
+               (println "[ws] open!"))
+    :on-message (fn [{:keys [_channel _data]}]
+                  (println "[ws] message!"))
     :on-error (fn [{_throwable :error}]
                 (println "[ws] error!"))
     :on-close (fn [_]
                 (println "[ws] close!"))}})
 
 (defroutes routes
-  (GET "ws" [] handle-ws))
+  (GET "/ws" [] handle-ws))
 
 (defn start []
   #_{:clj-kondo/ignore [:inline-def]}
